eb289225cc46c87f432ee3f22021cd4f527615d1
hakyeop <127179902+hakyeop@users.noreply.github.com>
2024-09-26 17:58:25 +0900

    Create 1085.c

---
+++ 1085.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int x, y, w, h;
+	scanf_s("%d%d%d%d", &x, &y, &w, &h);
+	int px, py, mx, my;
+	px = w - x;
+	py = h - y;
+	if (px <= x && px <= y && px <= py)
+		printf("%d\n", px);
+	else if (py <= x && py <= y && py <= px)
+		printf("%d\n", py);
+	else if (x <= px && x <= py && x <= y)
+		printf("%d\n", x);
+	else
+		printf("%d\n", y);
+}
