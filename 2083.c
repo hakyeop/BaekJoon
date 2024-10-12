421b710e9d9ab7d25df5b46988c7bad2a0cf73a3
hakyeop <127179902+hakyeop@users.noreply.github.com>
2024-10-13 00:33:18 +0900

    Create 2083.c

---
+++ 2083.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char name[11];
+    int age, weight;
+    while(1){
+        scanf("%s%d%d", name, &age, &weight);
+        if(name[0] == '#' && age==0 && weight==0)
+            break;
+        if(age > 17 || weight >= 80)
+            printf("%s Senior\n", name);
+        else
+            printf("%s Junior\n", name);
+    }
+}
