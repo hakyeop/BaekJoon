6993e2c6e41ab450f64a14e02c0f8238275704f9
hakyeop <127179902+hakyeop@users.noreply.github.com>
2024-10-26 12:29:46 +0900

    Create 19532.c

---
+++ 19532.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int a,b,c,d,e,f;
+    int x,y;
+    scanf("%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f);
+    for(int x=-999; x<=999; x++){
+        for(int y=-999; y<=999; y++){
+            if((a*x+b*y==c) && (d*x+e*y==f)){
+                printf("%d %d\n",x,y);
+                return 0;
+            }
+        }
+    }
+}
