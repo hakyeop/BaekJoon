75a7bca9e3f1ae61ae9aab2db9620387884b2a29
hakyeop <127179902+hakyeop@users.noreply.github.com>
2024-12-29 14:40:19 +0900

    Create 1927.java

---
+++ 1927.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+    	int n = input.nextInt();
+    	PriorityQueue<Integer> Heap = new PriorityQueue();
+    	for(int i=0; i<n; i++){
+        	int in=input.nextInt();
+        	if(in == 0){
+        		if(Heap.isEmpty()) {
+        			System.out.println("0");
+        		}
+        		else {
+        			System.out.println(Heap.poll());
+        		}
+        	}
+        	else {
+        		Heap.add(in);
+        	}
+    	}
+	}
+}
